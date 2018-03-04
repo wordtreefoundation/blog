adc889d4f8f57c905b8bb8423bdbbc7c7af2693f
Duane Johnson <duane.johnson@gmail.com>
2018-03-04 14:54:11 -0700

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# WordTree Foundation Blog
-
-Visit http://blog.wordtree.org
-
-## Setup
-
-Uses gatsby and gatsby-starter-lumen
---
+++ path---404-html-3251cb63e30160d7ec41.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xa2868bfb69fc],{377:function(e,t){e.exports={data:{site:{siteMetadata:{title:"WordTree Foundation Blog",subtitle:"The WordTree Foundation studies the relationships between books, with a special interest in LDS scripture such as the Book of Mormon.",copyright:"",menu:[{label:"Home",path:"http://wordtree.org/"},{label:"Articles",path:"/"},{label:"About WordTree",path:"/about/"}],author:{name:"WordTree Foundation",email:"info@wordtree.org",github:"wordtreefoundation"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-html-3251cb63e30160d7ec41.js.map
