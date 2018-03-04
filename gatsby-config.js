8b10ea3c9ac93a37338c46c6de6122f9185837ea
Duane Johnson <duane.johnson@gmail.com>
2018-03-04 15:13:13 -0700

    Add GA number

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' }
+      options: { trackingId: 'UA-35980893-3' }
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
